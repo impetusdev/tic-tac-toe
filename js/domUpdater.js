90e9840ddb3074e132f13e2bfb74acae9b167a11
Laurence Qi <laurence.qi@outlook.com>
2022-01-12 22:01:30 +1100

    added new computer move successfully

--- js/domUpdater.js
+++ js/domUpdater.js
@@ -13,9 +13,11 @@ $(function() {
     // setup click event for all grid squares. 
     $allGridSquares.on('click', function() {
         const $square = $(this);
-        const x = $square.data('x');
+        const x = $square.data('x'); // 
         const y = $square.data('y');
         gridClick();
+        //TODO: figure out why there is no visual update on the last human input (DOM not updating with "X")
+
 
         if (isWinner()) {
             const currentPlayer = ticInstance.currentTurn % 2 === 0 ? 1 : 2;
@@ -26,6 +28,39 @@ $(function() {
             alert('No one has won :(');
 
             $allGridSquares.unbind();
+        }
+
+        // if no winner, computer plays a move. 
+        // Initially computer will always be player two. 
+        ticInstance.playingComp = true;
+        if (ticInstance.playingComp) {
+            computerMove();
+        }
+
+        // click a random spot for player 2
+        function computerMove() {
+            let notPerformedMove = true;
+
+            while (notPerformedMove) {
+                let xRand = Math.floor(Math.random() * 3);
+                let yRand = Math.floor(Math.random() * 3);
+
+                if (ticInstance.occupiedSquares[yRand][xRand] == 'empty') {
+                    ticInstance.occupiedSquares[yRand][xRand] = 'O';
+                    ticInstance.incrementTurn();
+
+                    // console.log('all grid squares:', $allGridSquares);
+                    // console.log('the gridSquares[0] variable is:', $($allGridSquares[0]));
+                    const $current = $allGridSquares.filter(function() {
+                        console.log('the this is:', $(this));
+                        return $(this).data('x') === xRand && $(this).data('y') === yRand;
+                    });
+                    $current.text('O');
+                    // console.log('result of the filter:', $current);
+
+                    notPerformedMove = false;
+                }
+            }
         }
 
         function gridClick() {
@@ -57,6 +92,5 @@ $(function() {
                 return firstEl === 'empty' || !slice.every(square => square === firstEl); // this should result with true
             }); // the use of .every here is so that the loop will break when returning false
         }
-        // TODO: implement the computer mode response here. 
     });
 });
