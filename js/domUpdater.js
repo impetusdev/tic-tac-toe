e0a343a8c71a70edf55f0beede1b1483492e9696
Laurence Qi <laurence.qi@outlook.com>
2022-01-13 11:50:14 +1100

    moving isWinner to a higher level of abstraction

--- js/domUpdater.js
+++ js/domUpdater.js
@@ -26,49 +26,19 @@ $(function() {
         // setup click event for all grid squares. 
         $allGridSquares.on('click', function() {
             const $square = $(this);
+            const board = ticInstance.occupiedSquares;
             let x = $square.data('x');
             let y = $square.data('y');
             gridClick();
-            checkWinner();
+            checkWinner(x, y, board);
 
             //computer player 2 starts
             if (ticInstance.playingComp && ticInstance.currentTurn % 2 === 0) {
                 computerClick();
-                checkWinner();
+                checkWinner(x, y, board);
             }
 
-            function checkWinner() {
-                if (isWinner()) {
-                    const currentPlayer = ticInstance.currentTurn % 2 === 0 ? 1 : 2;
-                    $winner.text(`Player ${currentPlayer} has won!`);
-                    ticInstance.playingComp = false;
 
-                    $allGridSquares.unbind();
-                } else if (ticInstance.currentTurn > 9) {
-                    ticInstance.playingComp = false;
-                    $winner.text(`No one has won :()`);
-
-                    $allGridSquares.unbind();
-                }
-
-                // test if any row/col/diagonal that the clicked element lies on, is a match. 
-                function isWinner() {
-                    const slices = [];
-                    slices.push(
-                        ticInstance.occupiedSquares[y], // row
-                        ticInstance.occupiedSquares.map(row => row[x]), // col //TODO: consider putting in the if statement checking if the coords match the diagonal. 
-                        ticInstance.occupiedSquares.map((row, i) => row[i]), // diag starting top left
-                        ticInstance.occupiedSquares.map((row, i) => row[2 - i]), // diag starting top right
-                    );
-                    console.log('slices are: ', slices);
-
-                    return !slices.every(slice => { //return true if one slice is matching, else return false 
-                        console.log('slice', slice);
-                        const firstEl = slice[0];
-                        return firstEl === 'empty' || !slice.every(square => square === firstEl); // this should result with true
-                    }); // the use of .every here is so that the loop will break when returning false
-                }
-            }
             // click a random spot for player 2
             function computerClick() {
                 let notPerformedMove = true;
@@ -77,8 +47,8 @@ $(function() {
                     x = Math.floor(Math.random() * 3);
                     y = Math.floor(Math.random() * 3);
 
-                    if (ticInstance.occupiedSquares[y][x] == 'empty') {
-                        ticInstance.occupiedSquares[y][x] = 'O';
+                    if (board[y][x] == 'empty') {
+                        board[y][x] = 'O';
                         ticInstance.incrementTurn();
 
                         const $current = $allGridSquares.filter(function() {
@@ -96,9 +66,9 @@ $(function() {
                 //TODO: Implement a mode where in you check what the current ticInstance.playingComp and then generate the computer playing from there.  
                 const symbol = ticInstance.currentTurn % 2 === 1 ? 'X' : 'O';
 
-                if (ticInstance.occupiedSquares[y][x] === "empty") { //TODO: double check if the order of x and y is right. 
+                if (checkOccupied(x, y)) { //TODO: double check if the order of x and y is right. 
                     $square.text(symbol); // Updates DOM 
-                    ticInstance.occupiedSquares[y][x] = symbol; // Updates Model;
+                    board[y][x] = symbol; // Updates Model;
                     ticInstance.incrementTurn();
 
                 } else {
@@ -113,3 +83,39 @@ $(function() {
 function checkOccupied(x, y) {
     return ticInstance.occupiedSquares[y][x] === "empty";
 }
+
+function checkWinner(x, y, board) {
+    if (isWinner()) {
+        const currentPlayer = ticInstance.currentTurn % 2 === 0 ? 1 : 2;
+        $winner.text(`Player ${currentPlayer} has won!`);
+        ticInstance.playingComp = false;
+
+        $allGridSquares.unbind();
+    } else if (ticInstance.currentTurn > 9) {
+        ticInstance.playingComp = false;
+        $winner.text(`No one has won :()`);
+
+        $allGridSquares.unbind();
+    }
+
+    // test if any row/col/diagonal that the clicked element lies on, is a match. 
+    function isWinner() {
+        const slices = [];
+        slices.push(
+            board[y], // row
+            board.map(row => row[x]), // col //TODO: consider putting in the if statement checking if the coords match the diagonal. 
+            board.map((row, i) => row[i]), // diag starting top left
+            board.map((row, i) => row[2 - i]), // diag starting top right
+        );
+        console.log('slices are: ', slices);
+
+        return !slices.every(slice => { //return true if one slice is matching, else return false 
+            console.log('slice', slice);
+            const firstEl = slice[0];
+            return firstEl === 'empty' || !slice.every(square => square === firstEl); // this should result with true
+        }); // the use of .every here is so that the loop will break when returning false
+    }
+}
+
+// TODO: EXTRACT THE CHECKWINNER & ADD BOARD PARAM
+//
